74753b047a0c060ea7b4293a74244c60ce6db3ca
MJLEGION <m.george@alustudent.com>
2023-08-04 10:47:35 -0700

    1

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-# Takes in a URL, sends a GET request to the URL, and displays the body of the response
-
-url=$1
-response=$(curl -sI $url)
-status_code=$(echo "$response" | grep HTTP | awk '{print $2}')
-
-if [ $status_code -eq 200 ]; then
-    curl -sL $url
-else
-    echo "Status code: $status_code"
-fi
+# sends a GET request to the URL, and displays the body of the response
+curl -sfL "$1"
