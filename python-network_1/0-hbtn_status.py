5433c849001222e3a154229edb7fc9b0884cfd42
MJLEGION <m.george@alustudent.com>
2023-08-04 11:36:08 -0700

    0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-"""I document you"""
-
-
+""" Python script that fetches https://intranet.hbtn.io/status """
 import urllib.request
 
 
-if _name_ == '_main_':
+if _name_ == "_main_":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        content = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode("utf-8")))
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
