126f0da5a14cde80caa08b1506b15b0b805aba49
MJLEGION <m.george@alustudent.com>
2023-08-04 12:14:10 -0700

    4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-''' a Python script that fetches https://alx-intranet.hbtn.io/status
-'''
+""" a Python script that fetches https://intranet.hbtn.io/status """
 import requests
 
-if __name__ == "__main__":
-    response = requests.get('https://alx-intranet.hbtn.io/status')
-    print(f"Body response:\n\t- type: \
-{type(response.text)}\n\t- content: {response.text}")
+
+if __name__ == '__main__':
+    r = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type:", type(r.text))
+    print("\t- content:", r.text)
