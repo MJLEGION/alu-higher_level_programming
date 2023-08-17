3a51527c76d689a49cfcc2bb2010532cb0994440
MJLEGION <m.george@alustudent.com>
2023-08-17 08:20:17 -0700

    4

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,12 +1,31 @@
 #!/usr/bin/python3
-text_indentation = __import__('5-text_indentation').text_indentation
+'''
+Defines indentation function that indents text
+'''
 
-text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
-Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
-Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
-Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
-Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
-rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
-stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
-cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
-beatiorem! Iam ruinas videres""")
+
+def text_indentation(text):
+    '''
+    Function that prints text with two new lines
+    after each of these characters ., ? and :
+
+    Args:
+       text (str): The string to be printed
+    '''
+    if not(isinstance(text, str)):
+        raise TypeError("text must be a string")
+
+    i = 0
+    while(i < len(text) and text[i] == ' '):
+        i += 1
+
+    while i < len(text):
+        print(text[i], end="")
+        if text[i] == "\n" or text[i] in ".?:":
+            if text[i] in ('.', '?', ':'):
+                print('\n')
+            i += 1
+            while(i < len(text) and text[i] == ' '):
+                i += 1
+            continue
+        i += 1
