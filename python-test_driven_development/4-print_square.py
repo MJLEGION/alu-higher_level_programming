263a2343a12cf854d6fa644368e521ea3531e7e9
MJLEGION <m.george@alustudent.com>
2023-08-17 08:06:39 -0700

    3

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-print_square = __import__('4-print_square').print_square
+'''
+Function prints a square with the ``#`` character
+'''
 
-print_square(4)
-print("")
-print_square(10)
-print("")
-print_square(0)
-print("")
-print_square(1)
-print("")
-try:
-    print_square(-1)
-except Exception as e:
-    print(e)
-print("")
+
+def print_square(size):
+    '''
+    Prints a square with the # character based on size
+
+    Args:
+       size (int): The size of squares to be printed
+    '''
+    if not (isinstance(size, int)):
+        raise TypeError("size must be an integer")
+    if (size < 0):
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        print("#" * size)
