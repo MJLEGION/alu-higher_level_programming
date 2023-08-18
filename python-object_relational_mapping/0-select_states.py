dd433d0be6d84f985bb2a739092e983f03e86ff3
MJLEGION <m.george@alustudent.com>
2023-08-18 07:26:24 -0700

    done

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
-'''Lists all states from the database hbtn_0c_0_usa
-'''
+"""
+This script lists all states from the
+database `hbtn_0e_0_usa`.
+"""
 
 import MySQLdb
-import sys
+from sys import argv
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, db=sys.argv[3],
-                         user=sys.argv[1], passwd=sys.argv[2])
-    c = db.cursor()
-    c.execute("""SELECT * FROM states ORDER BY states.id ASC""")
-    for row in c.fetchall():
-        print(row)#!/usr/bin/python3
-'''Lists all states from the database hbtn_0c_0_usa
-'''
+if __name__ == '__main__':
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    db_connect = MySQLdb.connect(
+        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
 
-import MySQLdb
-import sys
+    db_cursor = db_connect.cursor()
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, db=sys.argv[3],
-                         user=sys.argv[1], passwd=sys.argv[2])
-    c = db.cursor()
-    c.execute("""SELECT * FROM states ORDER BY states.id ASC""")
-    for row in c.fetchall():
+    db_cursor.execute("SELECT * FROM states")
+
+    rows_selected = db_cursor.fetchall()
+
+    for row in rows_selected:
         print(row)
